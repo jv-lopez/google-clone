6f35cf285462066df3f3ac3a38c3c7f5228456d8
me-jameshwartlopez <me@jameshwartlopez.com>
2022-07-29 13:51:25 +0800

    create pagination components

--- pages/search.js
+++ pages/search.js
@@ -21,8 +21,9 @@ export default function search({ results }) {
 }
 
 export async function getServerSideProps(context) {
+	const startIndex = context.query.start || "1"
 	const mockData = false;
-	const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`).then((response) => response.json())
+	const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`).then((response) => response.json())
 
 	return {
 		props: {
